606ac42bf07d75ce5afff0197cf25166206e7660
Administrator <admin@cafintech.com>
2019-01-10 00:17:16 +0800

    api test ok

--- kong/plugins/quota-check/api.lua
+++ kong/plugins/quota-check/api.lua
@@ -5,19 +5,30 @@ return {
         before = function(self, dao_factory, helpers)
             crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
             self.params.consumer_id = self.consumer.id
-            self.params.custom_id = self.consumer.custom_id
+            self.params.custom_id = self.custom_id
+
+            local exist_quotas, err = crud.find_by_id_or_field(
+                    dao_factory.quota,
+                    { consumer_id = self.params.consumer_id },
+                    self.params.custom_id,
+                    "custom_id"
+            )
+
+            if err then
+                return helpers.yield_error(err)
+            elseif next(exist_quotas) == nil then
+                return helpers.responses.send_HTTP_NOT_FOUND()
+            end
+            self.params.username_or_id = nil
+            self.quota = exist_quotas[1]
         end,
 
         GET = function(self, dao_factory)
-            crud.get(self, dao_factory.quota)
+            crud.paginated_set(self, dao_factory.quota)
         end,
 
-        PUT = function(self, dao_factory, helpers)
-            crud.put(self.params, dao_factory.quota)
-        end,
-
-        POST = function(self, dao_factory, helpers)
-            crud.post(self.params, dao_factory.quota)
+        PATCH = function(self, dao_factory, helpers)
+            crud.patch(self.params, dao_factory.quota,self.quota)
         end
     }
 }
