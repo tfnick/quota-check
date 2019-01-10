b09ac65afba13ca0af306a848417350142605385
Administrator <admin@cafintech.com>
2019-01-10 09:34:49 +0800

    test

--- kong/plugins/quota-check/api.lua
+++ kong/plugins/quota-check/api.lua
@@ -5,7 +5,9 @@ return {
         before = function(self, dao_factory, helpers)
             crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
             self.params.consumer_id = self.consumer.id
-            self.params.custom_id = self.custom_id
+
+            --ngx.log(ngx.NOTICE," username_or_id ", self.consumer.custom_id)
+            self.params.custom_id = self.consumer.custom_id
 
             local exist_quotas, err = crud.find_by_id_or_field(
                     dao_factory.quota,
@@ -16,19 +18,33 @@ return {
 
             if err then
                 return helpers.yield_error(err)
-            elseif next(exist_quotas) == nil then
-                return helpers.responses.send_HTTP_NOT_FOUND()
             end
             self.params.username_or_id = nil
-            self.quota = exist_quotas[1]
+
+            -- judge is create or update
+            if next(exist_quotas) == nil then
+                self.quota = nil
+            else
+                self.quota = exist_quotas[1]
+            end
         end,
 
         GET = function(self, dao_factory)
             crud.paginated_set(self, dao_factory.quota)
         end,
 
-        PATCH = function(self, dao_factory, helpers)
-            crud.patch(self.params, dao_factory.quota,self.quota)
+        POST = function(self, dao_factory, helpers)
+            if self.quota == nil then
+                -- create
+                local res = crud.post(self.params, dao_factory.quota)
+                ngx.log(ngx.NOTICE,"create suceess ? ",res)
+            else
+                -- update
+                local res = crud.patch(self.params, dao_factory.quota, self.quota)
+                ngx.log(ngx.NOTICE,"create suceess ? ",res)
+            end
         end
     }
 }
+
+
